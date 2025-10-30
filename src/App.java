a9c4fe0c2d4a069c01e9ae12d1a83b008ff095f9
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-30 22:10:04 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int base = Integer.parseInt(System.console().readLine("Intrroduzca la base: "));
+        int expmax = Integer.parseInt(System.console().readLine("Intrroduzca el exponente máximo: "));
+        int exp = 1;
+        int sol = base;
+
+        for (int i = 0; i < expmax; i++) {
+            System.out.printf("%d^%d = %d%n", base, exp, sol);
+            exp++;
+            sol = sol * base;
+        }
     }
 }
